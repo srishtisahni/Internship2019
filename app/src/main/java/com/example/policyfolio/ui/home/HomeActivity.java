2fc051ccc772b353bf7d766ffcccae031205e193
Srishti Sahni <srishti.sahni16@gmail.com>
2019-07-12 17:13:15 +0530

    OTP Completed : Testing Pending

--- app/src/main/java/com/example/policyfolio/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/policyfolio/ui/home/HomeActivity.java
@@ -72,7 +72,6 @@ public class HomeActivity extends BaseNavigationActivity implements HomeCallback
         viewModel.getUser().observe(this, new Observer<User>() {
             @Override
             public void onChanged(@Nullable User user) {
-                Log.e("USER",user + "");
                 if(user!=null){
                     getTimeToast(user.getLastUpdated());
                     viewModel.updateUser(user);
@@ -91,7 +90,6 @@ public class HomeActivity extends BaseNavigationActivity implements HomeCallback
                                     Log.e("DOCUMENT VAULT","Error!");
                             }
                         });
-                        Log.e("INCOMPLETE",user.toString());
                         createBottomSheetForInfo();
                     }
                     else{
